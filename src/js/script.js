7566b62946e4157d4633f79a001c8661e6bd6bcb
ToshiSht <Shetinin_anton@bk.ru>
2022-10-09 18:53:34 +0300

    complead tabs

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,35 @@ const slider = tns({
   document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
   });
+
+
+    const tab = document.querySelectorAll('.catalog__tab');
+    tab.forEach((item,i) => {
+        item.addEventListener('click', e => {
+            e.preventDefault();
+            tab.forEach(item => {
+                item.classList.remove('catalog__tab_active');
+            });
+            tab[i].classList.add('catalog__tab_active');
+            document.querySelectorAll('.catalog__content').forEach(item => {
+                item.classList.remove('catalog__content_active')
+            });
+            document.querySelectorAll('.catalog__content')[i].classList.add('catalog__content_active');
+
+        }) 
+    });
+
+const itemLink = document.querySelectorAll('.catalog-item__link'),
+      backLink = document.querySelectorAll('.catalog-item__back')
+
+function toggleCart (toggleLink) {
+    toggleLink.forEach((item,i) => {
+        item.addEventListener('click', e => {
+            e.preventDefault();
+        document.querySelectorAll('.catalog-item__content')[i].classList.toggle('catalog-item__content_active');
+        document.querySelectorAll('.catalog-item__list')[i].classList.toggle('catalog-item__list_active');
+        })
+    });
+}
+toggleCart (itemLink);
+toggleCart (backLink);
